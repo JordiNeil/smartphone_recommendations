47641fdce231a52d6669cd7ca1aa893536a4c08b
Jordi Sanchez (Legion) <jordineil8@gmail.com>
2022-03-01 11:56:06 -0500

    added network, currency and frontend base

--- service/main.py
+++ service/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import crud, models, schemas
@@ -17,6 +18,14 @@ logger = logging.getLogger()
 logger.setLevel(logging.WARNING)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = SessionLocal()
